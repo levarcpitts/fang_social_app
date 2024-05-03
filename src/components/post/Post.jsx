c0e4e7211028e1b824d19d6967ad374edfa6d81b
“Levar <levar.pitts@gmail.com>
2024-05-02 23:28:34 -0400

    dynamic

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,32 +1,40 @@
 import "./post.css";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Users } from "../../dummyData";
 
-export default function Post() {
+export default function Post({post}) {
+
+const user = Users.filter(u=>u.id===1)
+
+console.log(user[0].username)
+
+console.log(user)
+
   return (
     <div className="post">
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img className="postProfileImg" src="assets/person/1.jpeg" alt="" />
-                    <span className="postUsername">John Doe</span>
-                    <span className="postDate">5 mins ago</span>
+                    <img className="postProfileImg" src={Users.filter(u=>u.id === post.userId)[0].profilePicture} alt="" />
+                    <span className="postUsername">{Users.filter(u=>u.id === post.userId)[0].username}</span>
+                    <span className="postDate">{post.date}</span>
 
                 </div>
                 <div className="postTopRight"></div>
             <MoreVertIcon />
             </div>
             <div className="postCenter">
-                <span className="postText">"Hey! its my first post!"</span>
-                <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+                <span className="postText">{post?.desc}</span>
+                <img className="postImg" src={post.photo} alt="" />
                 </div> 
             <div className="postBottom">
                 <div className="postBottomLeft">
                 <img className="likeIcon" src="/assets/like.png" />
                 <img className="likeIcon" src="/assets/heart.png" alt="" /> 
-                <span className="postLikeCounter">32 people like it</span>
+                <span className="postLikeCounter">{post.like}</span>
                 </div>
                 <div className="postBottomRight"></div>
-                <span className="postCommentText">9 comments</span>
+                <span className="postCommentText">{post.comment}</span>
             </div>
 
         </div>
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,23 @@
 import "./rightbar.css"
+import {Users} from "../../dummyData"
+import Online from "../online/Online"
 
 export default function Rightbar() {
   return (
     <div className="rightbar">
-        rightbar
+        <div className="rightbarWrapper">
+          <div className="birthdateContainer">
+            <img src="assets/gift.png" alt="" className="birthdayImg" />
+            <span className="birthdayText"><b>Pola Foster</b> and<b> 3 other friends</b> have a birthday today</span>
+          </div>
+          <img src="assets/ad.png" alt="" className="rightbarAd" /> 
+          <h4 className="rightbarTitle">Online Friends</h4>
+          <ul className="rightbarFriendList">
+            {Users.map(u=>(
+              <Online key={u.id} user={u} />
+            ))}
+          </ul>
+        </div>
     </div>
   )
 }
