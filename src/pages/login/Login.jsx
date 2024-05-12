aaa2d99ed3cfe100b42d0c6681d338a93dc18bf2
“Levar <levar.pitts@gmail.com>
2024-05-11 21:47:51 -0400

    Adding auth

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,34 @@
-import "./login.css"
+import { useRef, useContext } from "react";
+import "./login.css";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import { CircularProgress } from '@mui/material/';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  components: {
+    MuiCircularProgress: {
+      styleOverrides: {
+        root: {
+          color: 'white', 
+        },
+      },
+    },
+  },
+});
+
 
 export default function Login() {
+const email = useRef();
+const password = useRef();
+const {user, isFetching, error, dispatch} = useContext(AuthContext);
 
   const handleClick = (e) => {
     e.preventDefault();
-    console.log("clicker")
+    loginCall({email: email.current.value, password: password.current.value}, dispatch )
   }
+
+  console.log(user)
   return (
     <div className="login">
         <div className="loginWrapper">
@@ -15,11 +38,11 @@ export default function Login() {
         </div>
         <div className="loginRight">
             <form className="loginBox" onSubmit={handleClick}>
-                <input placeholder="Email" type="email" className="loginInput" />
-                <input placeholder="Password" type="password" className="loginInput" />
-                <button className="loginButton">Login</button>
+                <input placeholder="Email" type="email" className="loginInput" ref={email} required  />
+                <input placeholder="Password" type="password" className="loginInput" ref={password} required minLength="6" />
+                <ThemeProvider theme={theme}><button className="loginButton" type="submit" disabled={isFetching}>{isFetching ? <CircularProgress size="20px" color="primary" /> : "Login"}</button>
                 <span className="loginForgot">Forgot Password?</span>
-                <button className="loginRegister">Create a New Account</button>
+                <button className="loginRegister">{isFetching ? <CircularProgress size="20px" color="primary" /> : "Create New Account"}</button></ThemeProvider>
             </form>
         </div>
         </div>
